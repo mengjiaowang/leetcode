06bfa673c28bc8ec32767713b195468f842ea4bf
Mengjiao Wang <wmjdut@gmail.com>
2014-05-27 18:46:52 +0800

    simplify - merge-sort-array

--- merge-sorted-array.cpp
+++ merge-sorted-array.cpp
@@ -1,24 +1,11 @@
 class Solution {
     public:
         void merge(int A[], int m, int B[], int n) {
-            if(n == 0) return;
-            for(unsigned int i = 0; i != m; ++i)
-            {
-                A[m + n - 1 - i] = A[m - 1 - i];
-            }
-            unsigned int startA = n, startB = 0, index = 0;
-            while(startA != (m + n) && startB != n){
-                if(A[startA] <= B[startB]){
-                    A[index] = A[startA]; ++ startA; ++ index;
-                }else{
-                    A[index] = B[startB]; ++ startB; ++ index;
-                }
-            }
-            while(startA != (m + n)){
-                A[index] = A[startA]; ++ startA; ++index;
-            }
-            while(startB != n){
-                A[index] = B[startB]; ++ startB; ++ index;
+            for(int i = m + n - 1; i != -1; --i){
+                if(m <= 0)A[i] = B[--n];
+                else if(n <= 0)break;
+                else if(A[m-1] > B[n-1]) A[i] = A[--m];
+                else A[i] = B[--n];
             }
         }
 };
