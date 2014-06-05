bef51f4483e26ecbde7ff061ff95bdf4f8f89d3a
Mengjiao Wang <wmjdut@gmail.com>
2014-06-06 00:01:55 +0800

    a more beautiful add-binary

--- add-binary.cpp
+++ add-binary.cpp
@@ -1,57 +1,22 @@
 class Solution {
     public:
         string addBinary(string a, string b) {
-            int i = a.length() - 1, j = b.length() - 1;
-            string res; 
-            bool flag = false;
+            string::size_type m, n, max; 
+            m = a.size(); n = b.size();
+            max = (m > n) ? m : n;
+            int carry = 0; string res;
+            res.reserve(max + 1);
 
-            while(i >= 0 && j >= 0){
-                if(a[i] == '0' && b[j] == '0' && flag == false)
-                    res.push_back('0');
-                else if(a[i] == '0' && b[j] == '0' && flag == true){
-                    res.push_back('1'); flag = false;
-                }
-                else if(a[i] == '0' && b[j] == '1' && flag == false)
-                    res.push_back('1');
-                else if(a[i] == '0' && b[j] == '1' && flag == true)
-                    res.push_back('0');
-                else if(a[i] == '1' && b[j] == '0' && flag == false)
-                    res.push_back('1');
-                else if(a[i] == '1' && b[j] == '0' && flag == true)
-                    res.push_back('0');
-                else if(a[i] == '1' && b[j] == '1' && flag == false){
-                    res.push_back('0'); flag = true;
-                }
-                else if(a[i] == '1' && b[j] == '1' && flag == true)
-                    res.push_back('1');
-                i --; j --;
+            for(string::size_type i = 0; i != max; ++i){
+                int p = (i < m) ? a[m - 1 - i] - '0' : 0;
+                int q = (i < n) ? b[n - 1 - i] - '0' : 0;
+                int tmp = p + q + carry;
+                carry = tmp >> 1;
+                if(tmp & 1) res.push_back('1');
+                else res.push_back('0');
             }
-            while(i >= 0){
-                if(a[i] == '0' && flag == false)
-                    res.push_back('0');
-                else if(a[i] == '0' && flag == true){
-                    res.push_back('1'); flag = false;
-                }
-                else if(a[i] == '1' && flag == false)
-                    res.push_back('1');
-                else if(a[i] == '1' && flag == true)
-                    res.push_back('0');
-                i --;
-            }
-            while(j >= 0){
-                if(b[j] == '0' && flag == false)
-                    res.push_back('0');
-                else if(b[j] == '0' && flag == true){
-                    res.push_back('1'); flag = false;
-                }
-                else if(b[j] == '1' && flag == false)
-                    res.push_back('1');
-                else if(b[j] == '1' && flag == true)
-                    res.push_back('0');
-                j --;
-            }
-            if(flag == true)
-                res.push_back('1');
+            if(carry) res.push_back('1');
+
             std::reverse(res.begin(), res.end());
             return res;
         }
