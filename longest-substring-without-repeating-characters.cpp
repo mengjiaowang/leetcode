8b17b982361774b6f2b9234b2a644f6142f9b3f4
Mengjiao Wang <wmjdut@gmail.com>
2014-06-06 09:42:19 +0800

    replace std::set with integer array

--- longest-substring-without-repeating-characters.cpp
+++ longest-substring-without-repeating-characters.cpp
@@ -2,19 +2,37 @@ class Solution {
     public:
         int lengthOfLongestSubstring(string s) {
             int maxLength = 0;
-            std::set<char> substring;
-            for(int i = 0; i != s.size(); ++i){
-                for(int j = i; j != s.size(); ++j){
-                    if(substring.find(s[j]) != substring.end()){
-                        if(substring.size() > maxLength){
-                            maxLength = substring.size();
-                            substring.clear();
-                            break;
-                        } 
-                        else substring.insert(s[j]);
+            int substring[256];
+
+            memset((void*)substring, 0, 256*4);
+
+            for(int i = 0; i != s.size(); ++i)
+            {
+                for(int j = i; j != s.size(); ++j)
+                {
+                    if(substring[s[j]] == 1)
+                    {
+                        int length = 0;
+                        for(int k = 0; k != 256; ++ k)
+                        {
+                            if(substring[k]) length ++;
+                        }
+                        if(length > maxLength) maxLength = length;
+                        memset(substring, 0, 256*4);
+                        break;
+                    }
+                    else
+                    {
+                        substring[s[j]] = 1;
                     }
                 }
             }
+            int length = 0;
+            for(int k = 0; k != 256; ++ k)
+            {
+                if(substring[k]) length ++;
+            }
+            if(length > maxLength) maxLength = length;            
             return maxLength;
         }
 };
