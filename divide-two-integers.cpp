d7384d5f1d72c1ed40c16954f025cb0a213f31e8
Mengjiao Wang <wmjdut@gmail.com>
2014-06-16 15:10:17 +0800

    TLE was solved by a new algorithm

--- divide-two-integers.cpp
+++ divide-two-integers.cpp
@@ -1,41 +1,25 @@
-#include <iostream>
-using namespace std;
-
-int divide(int dividend, int divisor);
-
-int main()
-{
-    cout << divide(2147483647,2) << endl;
-    cout << divide(2147483647,2) << endl;
-    cout << divide(2147483647,2) << endl;
-    cout << divide(2147483647,2) << endl;
-    cout << divide(2147483647,2) << endl;
-    cout << divide(2147483647,2) << endl;
-    cout << divide(2147483647,2) << endl;
-    return 0;
-}
-
-int divide(int dividend, int divisor) {
-    int symbol = 1;
-    long n1 = dividend, n2 = divisor;
-    if(n1 < 0) {symbol = symbol * -1; n1 = n1 * -1;}
-    if(n2  < 0) {symbol = symbol * -1; n2 = n2 * -1;}
-
-    int res = 0, count = 0;
-    while(true){
-        if(n2 == 1) return n1*symbol;
-        if(n1 < n2) return res*symbol;
-        else if(n1 == n2) return (res + 1)*symbol;
-        else{
-            while(n1 > n2){
-                n2 = n2 << 1;
-                if(count == 0) count = 1;
-                else count = count << 1;
+class Solution {
+    public:
+        int divide(int dividend, int divisor) {
+            int sign = 1;
+            if((dividend < 0 && divisor > 0) || (dividend > 0 && divisor < 0)) sign = -1;
+            unsigned int a = dividend > 0 ? dividend : dividend * (-1);
+            unsigned int b = divisor  > 0 ? divisor  : divisor  * (-1);
+            unsigned int d = b, i = 0, res = 0;
+            unsigned int nums[32];
+            while(d <= a && d > 0){
+                nums[i++] = d;
+                d = d << 1;
             }
-            n1 = n1 - (n2 >> 1);
-            n2 = divisor;
-            res += count;
-            count = 0;
+            i --;
+            while(a >= b){
+                if(a >= nums[i]){
+                    a = a - nums[i];
+                    res += (1 << i);
+                }
+                i--;
+            }
+            return res*sign;
         }
-    }
-}
+};
+
