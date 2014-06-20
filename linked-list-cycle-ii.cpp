88527128b91197a2971988bce0b6b02f75da308e
Mengjiao Wang <wmjdut@gmail.com>
2014-06-21 00:06:04 +0800

    import new solutoin

---
+++ linked-list-cycle-ii.cpp
@@ -0,0 +1,23 @@
+class Solution {
+    public:
+        ListNode *detectCycle(ListNode *head) {
+            ListNode * p1 = head, *p2 = head;
+
+            while(p1 != NULL && p1->next != NULL){
+                p1 = p1->next->next;
+                p2 = p2->next;
+                if(p1 == p2) break;
+            }
+
+            if(p1 == NULL || p1->next == NULL)
+                return NULL;
+
+            p1 = head;
+
+            while(p1 != p2){
+                p1 = p1->next;
+                p2 = p2->next;
+            }
+            return p1;
+        }
+};
