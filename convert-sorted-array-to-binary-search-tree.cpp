752a480e0e6d9857ea7c097c7c26919a841ff3c2
Mengjiao Wang <wmjdut@gmail.com>
2014-06-26 19:56:39 +0800

    bug fixed and accepted by OJ

--- convert-sorted-array-to-binary-search-tree.cpp
+++ convert-sorted-array-to-binary-search-tree.cpp
@@ -1,35 +1,18 @@
-#include<iostream>
-#include<vector>
-using namespace std;
+class Solution {
+    public:
+        TreeNode *sortedArrayToBST(vector<int> &num) {
+            if(num.size() == 0) return NULL;
+            int start = 0, end = num.size() - 1;
+            TreeNode * root = createBST(num, start, end);
+            return root;
+        }
 
-struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+        TreeNode * createBST(vector<int> &num, int start, int end){
+            if(start > end) return NULL;
+            int mid = start - (start - end)/2;
+            TreeNode * root = new TreeNode(num[mid]);
+            root->left = createBST(num, start, mid-1);
+            root->right = createBST(num, mid+1, end);
+            return root;
+        }
 };
-
-void createBST(TreeNode * root, vector<int> &num, int start, int end);
-TreeNode *sortedArrayToBST(vector<int> &num);
-
-TreeNode *sortedArrayToBST(vector<int> &num) {
-    if(num.size() == 0) return NULL;
-    int start = 0, end = num.size() - 1;
-    TreeNode * root = NULL;
-    createBST(root, num, start, end);
-    return root;
-}
-
-void createBST(TreeNode * root, vector<int> &num, int start, int end){
-    if(start > end) return;
-    int mid = start - (start - end)/2;
-    root = new TreeNode(num[mid]);
-    createBST(root->left, num, 0, mid-1);
-    createBST(root->right, num, mid+1, end);
-}
-
-int main(){
-    int a[] = {1, 2, 3, 4, 5, 6};
-    vector<int> num(a, a+6);
-    TreeNode * root = sortedArrayToBST(num);
-}
