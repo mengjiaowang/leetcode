f49ec8023fbe7f42bf94eb6970f89331f79fb822
Mengjiao Wang <wmjdut@gmail.com>
2014-06-27 18:16:59 +0800

    add a O(n^2*lgn) solution, accepted

--- max-points-on-a-line.cpp
+++ max-points-on-a-line.cpp
@@ -2,24 +2,22 @@ class Solution {
     public:
         int maxPoints(vector<Point> &points) {
             int maxCount = 0;
+            if(points.size() == 0) return maxCount;
             for(int i = 0; i != points.size(); ++i){
-                for(int j = 0; j < i; ++j){
-                    int count = 0;
-                    for(int k = 0; k != points.size(); ++k){
-                        if(k != i && k !=j){
-                            if(points[i].x == points[j].x){
-                                if(points[k].x == points[i].x) count ++;
-                            }
-                            else if(points[i].y == points[j].y){
-                                if(points[k].y == points[i].y) count ++;
-                            }
-                            else{
-                                if((points[k].y-points[i].y)/(points[j].y-points[i].y)-(points[k].x-points[i].x)/(points[j].x-points[i].x) == 0) count ++;
-                            }
-                        }
-                    }
-                    if(count > maxCount) maxCount = count;
+                int count = 0, n = 0;
+                unordered_map<double, int> umap;
+                for(int j = 0; j != points.size(); ++j){
+                    int dx = points[j].x - points[i].x;
+                    int dy = points[j].y - points[i].y;
+                    double slope = std::numeric_limits<double>::infinity();
+                    if(dx == 0 && dy == 0){n ++; continue;}
+                    else if(dx != 0) slope = ((double)dy)/dx;
+                    int tmp = 0;
+                    if(umap.find(slope) != umap.end()) tmp = umap[slope];
+                    umap[slope] = tmp + 1;
+                    if(count < tmp + 1) count = tmp + 1;
                 }
+                if(maxCount < count + n) maxCount = count + n;
             }
             return maxCount;
         }
