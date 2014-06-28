450789ea1c71366ddaa7e2b67ed488ee2c0c7dd4
Mengjiao Wang <wmjdut@gmail.com>
2014-06-28 22:46:46 +0800

    add Kadane's algorithm O(n)

--- maximum-subarray.cpp
+++ maximum-subarray.cpp
@@ -1,13 +1,12 @@
-ass Solution {
+class Solution {
     public:
         int maxSubArray(int A[], int n) {
-            int maxSum = std::numeric_limits<int>::min();
-            for(int i = 0; i != n; ++ i){
-                int sum = 0;
-                for(int j = i; j != n; ++j){
-                    sum += A[j];
-                }
-                if(sum > maxSum) maxSum = sum;
+            if(n <= 0) return 0;
+            int maxSum = A[0], maxEnding = A[0];
+            for(int i = 1; i != n; ++i){
+                if(maxEnding < 0) maxEnding = A[i];
+                else maxEnding += A[i];
+                if(maxSum < maxEnding) maxSum = maxEnding;
             }
             return maxSum;
         }
