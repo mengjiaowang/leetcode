ee18564dd935a235bdd02cfbcd69eb69d7b5f8e9
Mengjiao Wang <wmjdut@gmail.com>
2014-07-06 11:35:47 +0800

    a more simplified code

--- permutation-sequence.cpp
+++ permutation-sequence.cpp
@@ -5,27 +5,18 @@ class Solution {
             bool used[n];
             memset((void*)used, false, sizeof(bool)*n);
             for(int i = 0; i != n; ++i){
-                int count = 0, index = 0;
-                if(k == 1){
-                    for(index = 0; index != n; ++index){
-                        if(used[index]==false) permutation.push_back('0'+index+1);
-                    }
-                    break;
+                int count = 0, index = 0, factorial = 1;
+                for(int j = 1; j <= n-i-1; ++j){
+                    factorial *= j;
                 }
-                else{
-                    int f = 1;
-                    for(int j = 1; j <= n-i-1; ++j){
-                        f *= j;
-                    }
-                    int v = (k-1)/f + 1; 
-                    for(index = 0; index != n; ++index){
-                        if(used[index] == false) count ++;
-                        if(count == v) break;
-                    }
-                    used[index] = true;
-                    permutation.push_back('0'+index+1);
-                    k = (k-1) % f + 1;
+                int v = (k-1)/factorial + 1; 
+                for(index = 0; index != n; ++index){
+                    if(used[index] == false) count ++;
+                    if(count == v) break;
                 }
+                used[index] = true;
+                permutation.push_back('0'+index+1);
+                k = (k-1) % factorial + 1;
             }
             return permutation;
         }
