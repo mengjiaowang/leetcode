79b8ae0379d05422ad50ba5b4da555192cf26b3a
Mengjiao Wang <wmjdut@gmail.com>
2014-07-08 22:44:18 +0800

    add plus one

---
+++ plus-one.cpp
@@ -0,0 +1,15 @@
+class Solution {
+    public:
+        vector<int> plusOne(vector<int> &digits) {
+            vector<int> res; int flag = 0;
+            for(int i = digits.size() - 1; i >= 0; --i){
+                int tmp = digits[i] + flag;
+                if(i == digits.size() - 1) tmp ++;
+                if(tmp < 10) {res.push_back(tmp); flag = 0;}
+                else {res.push_back(tmp-10); flag = 1;}
+            }
+            if(flag == 1) res.push_back(1);
+            reverse(res.begin(), res.end());
+            return res;
+        }
+};
