efa4ee19ecb13732736ca114548f77834d5da541
Mengjiao Wang <wmjdut@gmail.com>
2014-07-11 14:08:42 +0800

    add a merge sort solution, accepted

--- sort-list.cpp
+++ sort-list.cpp
@@ -1,43 +1,66 @@
 class Solution {
     public:
-        ListNode *sortList(ListNode *head) {
-            if(head == NULL) return NULL;
-            return sortList(head, NULL);
-        }
 
-        ListNode *sortList(ListNode *head, ListNode *end){
-            if(head != end && head != NULL){
-                ListNode * mid = head;
-                ListNode * newHead = getPartitionHead(head, end);
-                ListNode * first = sortList(newHead, mid);
-                if(mid->next != NULL){
-                    ListNode * second = sortList(mid->next, end);
-                    mid->next = second;
-                }
-                return first;
+        ListNode *sortList(ListNode *head){
+            if(head != NULL && head->next != NULL){
+                ListNode * mid = findMid(head);
+                ListNode * first = sortList(head);
+                ListNode * second = sortList(mid);
+                ListNode * merged = mergeLists(first, second); 
+                return merged;
             }
             else return head;
         }
 
-        ListNode *getPartitionHead(ListNode *head, ListNode *end){
-            ListNode *first = NULL, *curFirst = first;
-            ListNode *second = NULL, *curSecond = first;
-            ListNode *cur = head->next;
-            while(cur != end && cur != NULL){
-                if(cur->val < head->val){
-                    if(first == NULL){ first = cur; curFirst = cur;}
-                    else{ curFirst->next = cur; curFirst = cur;}
-                }else{
-                    if(second == NULL){ second = cur; curSecond = cur;}
-                    else{ curSecond->next = cur; curSecond = cur;}
-                }
-                cur = cur->next;
+        ListNode *findMid(ListNode *head){
+            ListNode *p1 = head, *p2 = head;
+            while(p2 != NULL && p2->next != NULL){
+                p2 = p2->next->next;
+                if(p2 != NULL) p1 = p1->next;
             }
-            if(curFirst != NULL){ curFirst->next = head; head->next = second;}
-            else { first = head; first->next = second;}
-            if(second != NULL) { curSecond->next = end;}
-            else head->next = end;
-            return first;
+            p2 = p1->next;
+            p1->next = NULL;
+            return p2;
+        }
+
+        ListNode *mergeLists(ListNode *head1, ListNode *head2){
+            if(head1 == NULL) return head2;
+            if(head2 == NULL) return head1;
+            ListNode *curHead1 = head1, *curHead2 = head2;
+            ListNode *head = NULL, *cur = NULL;
+            while(curHead1 != NULL && curHead2 != NULL){
+                if(curHead1->val < curHead2->val){
+                    if(head == NULL){
+                        head = curHead1; cur = head;
+                    }
+                    else{
+                        cur->next = curHead1; cur = cur->next;
+                    }
+                    curHead1 = curHead1->next;
+                }
+                else{
+                    if(head == NULL){
+                        head = curHead2; cur = head;
+                    }
+                    else{
+                        cur->next = curHead2; cur = cur->next;
+                    }
+                    curHead2 = curHead2->next;
+                }
+            }
+
+            while(curHead1 != NULL){
+                cur->next = curHead1;
+                cur = cur->next;
+                curHead1 = curHead1->next;
+            }
+
+            while(curHead2 != NULL){
+                cur->next = curHead2;
+                cur = cur->next;
+                curHead2 = curHead2->next;
+            }
+            return head; 
         }
 
 };
