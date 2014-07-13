bec6d1969cc1818b898460b8cda633e0635d18d6
Mengjiao Wang <wmjdut@gmail.com>
2014-07-13 20:31:16 +0800

    bug fixed, solved

--- lru-cache.cpp
+++ lru-cache.cpp
@@ -2,30 +2,49 @@ class LRUCache{
     public:
         LRUCache(int capacity) {
             this->capacity = capacity;
-            this->count = 0;
         }
 
         int get(int key) {
-            unordered_map<int, int>::iterator i = mapping.find(key);
-            if(i != mapping.end()) return i->second;
+            unordered_map<int, int>::iterator i = cache.find(key);
+            if(i != cache.end()){
+                queue.erase(mapping[key]);
+                queue.push_back(key);
+                list<int>::iterator last = queue.end(); last --;
+                mapping[key] = last;
+                return i->second;
+            } 
             else return -1;
         }
 
         void set(int key, int value) {
-            if(keyList.size() < capacity){
-                keyList.push_back(key);
-                mapping[key] = value;
+            unordered_map<int, int>::iterator i = cache.find(key);
+            if(i == cache.end()){
+                if(cache.size() < this->capacity){
+                    cache[key] = value;
+                    queue.push_back(key);
+                    list<int>::iterator last = queue.end(); last --;
+                    mapping[key] = last;
+                }
+                else{
+                    cache.erase(queue.front());
+                    mapping.erase(queue.front());
+                    queue.pop_front();
+                    cache[key] = value;
+                    queue.push_back(key);
+                    list<int>::iterator last = queue.end(); last --;
+                    mapping[key] = last; 
+                }
             }else{
-                int oldKey = keyList.front();
-                keyList.pop_front();
-                mapping.erase(oldKey);
-                keyList.push_back(key);
-                mapping[key] = value;
+                cache[key] = value;
+                queue.erase(mapping[key]);
+                queue.push_back(key);
+                list<int>::iterator last = queue.end(); last --;
+                mapping[key] = last;
             }
         }
 
-        unordered_map<int, int> mapping;
-        list<int> keyList;
+        unordered_map<int, int> cache;
+        unordered_map<int, list<int>::iterator> mapping;
+        list<int> queue;
         int capacity;
-        int count;
 };
