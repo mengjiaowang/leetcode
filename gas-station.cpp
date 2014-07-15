6c4cf4e981ae406458bcf6e2390f91cacdd81a96
Mengjiao Wang <wmjdut@gmail.com>
2014-07-15 20:57:35 +0800

    a O(n) solution from discussion, accepted

--- gas-station.cpp
+++ gas-station.cpp
@@ -1,17 +1,14 @@
 class Solution {
     public:
         int canCompleteCircuit(vector<int> &gas, vector<int> &cost) {
-            int n = gas.size();
+            int total = 0, tank = 0, start = 0;
             for(int i = 0; i != gas.size(); ++i){
-                int count = n, j = i, left = 0;
-                while(count > 0){
-                    left += gas[j] - cost[j];
-                    if(left < 0) break;
-                    ++j;--count;
-                    if(j == n) j = 0;
+                tank += gas[i] - cost[i];
+                if(tank < 0){
+                    start = i + 1; total += tank; tank = 0;
                 }
-                if(j == i && left >= 0) return i;
             }
-            return -1;
+            if(tank + total < 0) return -1;
+            else return start;
         }
 };
