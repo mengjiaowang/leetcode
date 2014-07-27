4a0f776a3922f5f2ad010e382821cbe9fe1f4b19
Mengjiao Wang <wmjdut@gmail.com>
2014-07-27 19:57:43 +0800

    add a recirsive solution O(logn), accepted

--- powx-n.cpp
+++ powx-n.cpp
@@ -6,12 +6,25 @@ class Solution {
             if(n < 0){
                 n = (-1)*n;
                 flag = true;
-            } 
-            for(int i = 0; i != n; ++i){
-                res *= x;
             }
+
+            res = positivePow(x, n);
+
             if(flag == false)
                 return res;
             else return 1/res;
+        }
+
+        double positivePow(double x, int n){
+            if(n == 0) return 1;
+            else if(n == 1) return x;
+            else if(n % 2 == 1){
+                double tmp = positivePow(x,n/2);
+                return x*tmp*tmp;
+            }
+            else{
+                double tmp = positivePow(x,n/2);
+                return tmp*tmp;            
+            }
         }
 };
