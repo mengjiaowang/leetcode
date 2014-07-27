98967ba9252e3b9c95bc98b7c52b93c91d58d4ee
Mengjiao Wang <wmjdut@gmail.com>
2014-07-27 19:48:27 +0800

    bug fixed, but still TLE

--- powx-n.cpp
+++ powx-n.cpp
@@ -2,9 +2,16 @@ class Solution {
     public:
         double pow(double x, int n) {
             double res = 1;
+            bool flag = false;
+            if(n < 0){
+                n = (-1)*n;
+                flag = true;
+            } 
             for(int i = 0; i != n; ++i){
                 res *= x;
             }
-            return res;
+            if(flag == false)
+                return res;
+            else return 1/res;
         }
 };
