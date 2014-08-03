c9553e51ddc0a14dd75bac1bcf80f3c240014af8
Mengjiao Wang <wmjdut@gmail.com>
2014-08-03 22:01:46 +0800

    add a O(n) time and constant space solution

--- container-with-most-water.cpp
+++ container-with-most-water.cpp
@@ -1,13 +1,21 @@
 class Solution {
     public:
         int maxArea(vector<int> &height) {
+            int left = 0, right = height.size() - 1;
             int max = 0;
-            for(int i = 0; i != height.size(); ++i){
-                for(int j = i + 1; j < height.size(); ++j){
-                    int s = (j-i)*std::abs(height[j]-height[i]);
-                    if(s > max) max = s;
+            while(left < right){
+                int area = (right - left) * min(height[left], height[right]);
+                if(area > max) max = area;
+                if(height[left] > height[right]){
+                    right --;
+                }else{
+                    left ++;
                 }
             }
             return max;
+        }
+
+        inline int min(int a, int b){
+            return a < b ? a : b;
         }
 };
