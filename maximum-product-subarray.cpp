33732414610114100692a7e04138ff82f0caf992
Mengjiao Wang <wmjdut@gmail.com>
2014-09-24 20:32:15 +0800

    add a O(n) solution, accepted

--- maximum-product-subarray.cpp
+++ maximum-product-subarray.cpp
@@ -2,16 +2,27 @@ class Solution {
     public:
         int maxProduct(int A[], int n) {
             if(n == 0) return 0;
-            int max = A[0];
-            int *p = new int[n];
+            int pos = 0, neg = 0, ans = A[0];
+
             for(int i = 0; i != n; ++i){
-                for(int j = i; j != n; ++j){
-                    if(j == i) p[j] = A[j];
-                    else p[j] = p[j-1]*A[j];
-                    if(p[j]>max) max = p[j];
+                if(A[i] > 0){
+                    if(pos > 0) pos = pos*A[i];
+                    else pos = A[i];
+                    neg = neg*A[i];
                 }
+                else if(A[i] < 0){
+                    int tmp = neg;
+                    if(pos > 0) neg = pos*A[i];
+                    else neg = A[i];
+                    pos = tmp * A[i];
+                }
+                else if(A[i] == 0){
+                    pos = 0;
+                    neg = 0;
+                }
+                if(pos > 0 || A[i] == 0)
+                    ans = max(ans, pos);
             }
-            delete p;
-            return max;
+            return ans;
         }
 };
