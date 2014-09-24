00d1011bf0dfc924be0eae5a40e050d73b9fdbe4
Mengjiao Wang <wmjdut@gmail.com>
2014-09-24 19:47:28 +0800

    add a solution from discussion

--- search-in-rotated-sorted-array-ii.cpp
+++ search-in-rotated-sorted-array-ii.cpp
@@ -1,49 +1,23 @@
 class Solution {
     public:
         bool search(int A[], int n, int target) {
-            if(n == 0) return -1;
-            int start = findStart(A, n);
-            if(start == 0){
-                int index = binarySearch(A, 0, n - 1, target);
-                if(index == -1) return false;
-                else return true;
-            }
-            else{
-                int index = binarySearch(A, 0, start - 1, target);
-                if(index != -1) return true;
-                index = binarySearch(A, start, n - 1, target);
-                if(index != -1) return true;
-                else return false;
-            }        
-        }
-
-        int findStart(int A[], int n){
-            if(n < 2) return 0;
-            int start = 0, end = n - 1;
-            while(end - start > 1){
-                int mid = (start + end)/2;
-                if(A[mid] >= A[start] && A[mid] <= A[end]) return start;
-                else if(A[mid] > A[start] && A[mid] > A[end]) start = mid;
-                else if(A[mid] < A[start] && A[mid] < A[end]) end = mid;
-                else if(A[mid] == A[start] && A[mid] == A[end]) {
-                    if(start == mid - 1 && end == mid + 1)
-                        return start;
-                    else start = mid;
+            int l = 0, r = n - 1;
+            while(l <= r){
+                int m = l + (r-l)/2;
+                if(A[m] == target) return true;
+                else if(A[l] < A[m]){ // left part is sorted
+                    if(A[l] <= target && target < A[m]) r = m - 1;
+                    else l = m + 1;
                 }
-                else if(A[mid] < A[start] && A[mid] == A[end]) end = mid;
-                else if(A[mid] == A[start] && A[mid] > A[end]) start = mid;
+                else if(A[m] < A[r]){ // right part is sorted
+                    if(A[m] < target && target <= A[r]) l = m + 1;
+                    else r = m - 1;
+                }
+                else{
+                    if(A[l] == target) return true;
+                    else l++;
+                };
             }
-            if(A[start] < A[end]) return start;
-            else return end;
-        }
-
-        int binarySearch(int A[], int start, int end, int target){
-            while(start <= end){
-                int mid = (start + end)/2;
-                if(A[mid] == target) return mid;
-                else if(A[mid] > target) end = mid - 1;
-                else if(A[mid] < target) start = mid + 1;
-            }
-            return -1;
+            return false;
         }
 };
