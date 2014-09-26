461ae1693a8147a28e59de5fe92ecebeee3cc7c6
Mengjiao Wang <wmjdut@gmail.com>
2014-09-26 12:06:20 +0800

    simplified code, but same idea

--- zigzag-conversion.cpp
+++ zigzag-conversion.cpp
@@ -5,27 +5,17 @@ class Solution {
             if(nRows == 1) return s;
             string result;
             result.reserve(s.size());
-            int index = 0;
-            while(index < s.size()){
-                result.push_back(s[index]);
-                index += (nRows+nRows-2);
-            }
-
-            for(int i = 1; i != nRows-1; ++i){
-                index = i;
+            for(int i = 0; i != nRows; ++i){
+                int index = i;
                 while(index < s.size()){
                     result.push_back(s[index]);
-                    int tmpIndex = index + (2*(nRows-i-1));
-                    if(tmpIndex < s.size())
-                        result.push_back(s[tmpIndex]);
+                    if(i != 0 && i != nRows-1){
+                        int tmpIndex = index + (2*(nRows-i-1));
+                        if(tmpIndex < s.size())
+                            result.push_back(s[tmpIndex]);
+                    }
                     index += (nRows+nRows-2);
                 }
-            }
-
-            index = nRows - 1;
-            while(index < s.size()){
-                result.push_back(s[index]);
-                index += (nRows+nRows-2);
             }
             return result;
         }
