a79d65478ecf80b7b63f2e00ff3170ddcf26619f
Mengjiao Wang <mengjiaowang@users.noreply.github.com>
2014-10-06 14:55:22 +0800

    a simplified solution

--- best-time-to-buy-and-sell-stock.cpp
+++ best-time-to-buy-and-sell-stock.cpp
@@ -1,21 +1,14 @@
 class Solution {
-    public:
-        int maxProfit(vector<int> &prices) {
-            return maxProfit(prices, 0);
-        }
-
-        int maxProfit(vector<int> & prices, int start){
-            int max = 0;
-            if(prices.size() - start < 2) return max;
-            int minPrice = prices[start];
-            for(int i = 1+start; i != prices.size(); ++i){
-                if(prices[i] > minPrice) max = max > (prices[i] - minPrice) ? max:(prices[i] - minPrice);
-                else if(prices[i] < minPrice){
-                    int profit = maxProfit(prices, i);
-                    max = max > profit ? max : profit; 
-                    return max;
-                }
+public:
+    int maxProfit(vector<int> &prices) {
+        if(prices.size() < 2) return 0;
+        int min = prices[0], profit = 0;
+        for(int i = 1; i != prices.size(); ++i){
+            if(prices[i] < min) min = prices[i];
+            else{
+                if(prices[i] - min > profit) profit = prices[i] - min;
             }
-            return max;
         }
+        return profit;
+    }
 };
