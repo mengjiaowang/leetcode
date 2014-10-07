e92de85dd1c84142d14a9217760cfbbf033a5c82
Mengjiao Wang <wmjdut@gmail.com>
2014-10-07 15:56:23 +0800

    use multimap instead of linear search

--- merge-k-sorted-lists.cpp
+++ merge-k-sorted-lists.cpp
@@ -1,31 +1,28 @@
 class Solution {
     public:
         ListNode *mergeKLists(vector<ListNode *> &lists) {
-            ListNode * head = NULL;
-            ListNode * cur = NULL;
-            vector<ListNode*> headers(lists);
-            head = findMinAndUpdate(headers);
-            cur = head;
-            while(cur != NULL){
-                cur->next = findMinAndUpdate(headers);
-                cur = cur->next;
+            ListNode * head = NULL, *cur = NULL;
+            if(lists.size() == 0) return head;
+            multimap<int, ListNode*> heap;
+            for(int i = 0; i != lists.size(); ++i){
+                if(lists[i] != NULL)
+                    heap.insert(pair<int, ListNode*>(lists[i]->val,lists[i]));
+            }
+            multimap<int, ListNode*>::iterator min;
+            while(!heap.empty()){
+                min = heap.begin();
+                if(head == NULL){
+                    head = min->second;
+                    cur = min->second;
+                }else{
+                    cur->next = min->second;
+                    cur = cur->next;
+                }
+                if(min->second->next != NULL){
+                    heap.insert(pair<int, ListNode*>(min->second->next->val,min->second->next));
+                }
+                heap.erase(min);
             }
             return head;
-        }
-
-        ListNode *findMinAndUpdate(vector<ListNode *> &headers) {
-            int minValue = std::numeric_limits<int>::max();
-            ListNode * minPointer = NULL;
-            int minIndex = -1;
-            for(int i = 0; i != headers.size(); ++i){
-                if(headers[i] == NULL) continue;
-                if(headers[i]->val < minValue){
-                    minValue = headers[i]->val;
-                    minPointer = headers[i];
-                    minIndex = i;
-                }
-            }
-            if(minIndex != -1) headers[minIndex] = headers[minIndex]->next;
-            return minPointer;
         }
 };
