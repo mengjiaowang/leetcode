37c39d3050f876893c226d5d9697e297966d7c49
Mengjiao Wang <wmjdut@gmail.com>
2014-10-09 18:37:49 +0800

    remove duplicates without using set. still O(n^2)

--- 3sum.cpp
+++ 3sum.cpp
@@ -2,35 +2,31 @@ class Solution {
     public:
         vector<vector<int> > threeSum(vector<int> &num) {
             vector<vector<int> > result;
-            set<vector<int> > dup;
             sort(num.begin(), num.end());
             for(int i = 0; i != num.size(); ++i){
-                int start = 0, end = num.size() - 1;
-                if(start == i) ++start;
-                if(end == i) --end;
+                if(i != 0 && num[i] == num[i-1]) continue;
+                int start = i + 1, end = num.size() - 1;
                 while(start < end){
-                    if(num[start] + num[end] == num[i]*-1){
+                    if(start != i + 1 && num[start] == num[start-1]){
+                        ++start;
+                        continue;
+                    }
+                    int sum = num[start] + num[end];
+                    if(sum == num[i]*-1){
                         vector<int> tmp;
                         tmp.push_back(num[start]);
                         tmp.push_back(num[end]);
                         tmp.push_back(num[i]);
                         sort(tmp.begin(), tmp.end());
-                        if(dup.find(tmp) == dup.end()){
-                            result.push_back(tmp);
-                            dup.insert(tmp);
-                        }
+                        result.push_back(tmp);
                         ++start;
-                        if(start == i) ++start;
                         --end;
-                        if(end == i) --end;
                     }
-                    else if(num[start] + num[end] < num[i]*-1){
+                    else if(sum < num[i]*-1){
                         ++start;
-                        if(start == i) ++start;
                     }
                     else{
                         --end;
-                        if(end == i) --end;
                     }
                 }
             }
