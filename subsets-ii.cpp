8be208030bfa42face7b1ec7dca735fe7f2de8e2
Mengjiao Wang <mengjiao.wang@sap.com>
2014-10-11 14:50:40 +0800

    dfs solution for subset generation problems

--- subsets-ii.cpp
+++ subsets-ii.cpp
@@ -1,6 +1,6 @@
 class Solution {
     public:
-        vector<vector<int> > subsets(vector<int> &S) {
+        vector<vector<int> > subsetsWithDup(vector<int> &S) {
             vector<vector<int> > result;
             vector<int> path;
             sort(S.begin(), S.end());
@@ -8,9 +8,10 @@ class Solution {
             return result;
         }
 
-        void dfs(vector<int>&S, vector<int>::iterator begin, vector<int> &path, vector<vector<int> > &result){
+        void dfs(vector<int> &S, vector<int>::iterator begin, vector<int> &path, vector<vector<int> > &result){
             result.push_back(path);
             for(vector<int>::iterator i = begin; i != S.end(); i++){
+                if(i != begin && *i == *(i-1)) continue;
                 path.push_back(*i);
                 dfs(S, i+1, path, result);
                 path.pop_back();
