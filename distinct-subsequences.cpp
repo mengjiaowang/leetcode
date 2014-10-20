b7f5b544aca7b0ad52ec887aa35780ee5a8e7538
Mengjiao Wang <mengjiao.wang@sap.com>
2014-10-20 12:55:22 +0800

    add a DP solution, accepted

--- distinct-subsequences.cpp
+++ distinct-subsequences.cpp
@@ -1,14 +1,19 @@
 class Solution {
     public:
         int numDistinct(string S, string T) {
-            if(T.size() == 0) return 1;
-            if(S.size() < T.size()) return 0;
+            int *matrix = new int[T.size()+1];
+            memset((void*)matrix, 0, sizeof(int)*(T.size()+1));
+            matrix[0] = 1;
             int count = 0;
-            for(int i = 0; i != S.size(); ++i){
-                if(T[0] == S[i]){
-                    count += numDistinct(S.substr(i+1), T.substr(1));
+            for(int i = 1; i != S.size()+1; ++i){
+                for(int j = T.size(); j >= 1; --j){
+                    if(S[i-1] == T[j-1]){
+                        matrix[j] += matrix[j-1];
+                    }
                 }
             }
+            count = matrix[T.size()];
+            delete []matrix;
             return count;
         }
 };
